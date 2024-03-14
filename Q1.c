a32b45b57467f06513c99b1cc3e9faa4dc5e58d8
amrut7070 <jadhavamrut982@gmail.com>
2024-03-14 23:10:33 +0530

    Q1 to Q4

---
+++ Q1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+    int num1, num2;
+
+	    printf("Enter two numbers : ");
+        scanf("%d %d", &num1, &num2);
+
+  
+        if(num1>num2)
+		   printf("num1 is greater");
+         
+        else
+		   printf("num2 is greater");    
+    
+}
